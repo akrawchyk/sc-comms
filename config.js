952af3160c9107a1bce3c0241ba36711abf2c421
Andrew Krawchyk <akrawchyk@gmail.com>
2016-09-23 11:55:18 -0400

    setup example for bull job queue

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
+const fs = require('fs')
 const os = require('os')
-const path = requre('path')
+const path = require('path')
 const convict = require('convict')
 
 const config = convict({
@@ -9,20 +10,32 @@ const config = convict({
     default: 'development',
     env: 'NODE_ENV'
   },
-  host: {
-    doc: 'The host name to bind to',
+  httpHost: {
+    doc: 'The host name to bind to for HTTP server',
     format: String,
     default: '0.0.0.0',
-    env: 'HOST'
-  }
-  port: {
-    doc: 'The port to bind to',
+    env: 'HTTP_HOST'
+  },
+  httpPort: {
+    doc: 'The port to bind to for HTTP server',
     format: 'port',
     default: 8000,
-    env: 'PORT'
+    env: 'HTTP_PORT'
+  },
+  redisHost: {
+    doc: 'The host name to connect to for Redis server',
+    format: String,
+    default: '0.0.0.0',
+    env: 'REDIS_HOST'
+  },
+  redisPort: {
+    doc: 'The port to connect to for Redis server',
+    format: 'port',
+    default: 6379,
+    env: 'REDIS_PORT'
   },
   workers: {
-    doc: 'The number of worker processes'
+    doc: 'The number of worker processes',
     format: 'nat',
     default: os.cpus().length || 1,
     env: 'SC_WORKERS'
@@ -35,7 +48,18 @@ const config = convict({
   }
 })
 
-config.loadFile(path.join(__dirname, '.env.json'))
+const configFile = path.join(__dirname, '.env.json')
+fs.open('myfile', 'r', (err, fd) => {
+  if (err) {
+    if (err.code === 'ENOENT') {
+      return
+    } else {
+      throw err
+    }
+  }
+
+  config.loadFile(configFile)
+})
 
 config.validate({ strict: true })
 
